f562031d0d1e90c6aaa825cb42977b0c9a1a913b
MateusHBR <mateusmorais.r@gmail.com>
2020-07-10 19:00:39 -0300

    criando tela home da loja

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
-  const HomePage({Key key, this.title = "Home"}) : super(key: key);
+  const HomePage({Key key, this.title = "Marvel Store"}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -15,12 +17,84 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    Color backgroundColor = Theme.of(context).backgroundColor;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
+      backgroundColor: backgroundColor,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            floating: true,
+            title: Text(
+              widget.title,
+              style: TextStyle(color: Colors.black),
+            ),
+            elevation: 0,
+            backgroundColor: backgroundColor,
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.shopping_cart,
+                  color: Colors.black,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          Observer(
+            builder: (_) {
+              if (controller.heroFuture.error != null) {
+                return SliverFillRemaining(
+                  child: Center(
+                    child: Text('ERRO'),
+                  ),
+                );
+              }
+              if (controller.heroFuture.status == FutureStatus.pending) {
+                return SliverFillRemaining(
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              }
+              return SliverPadding(
+                padding: EdgeInsets.all(8),
+                sliver: SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (ctx, index) {
+                      return Center(
+                        child: Column(
+                          children: <Widget>[
+                            Card(
+                              margin: EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 15,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              elevation: 10,
+                              child: Container(
+                                width: double.infinity,
+                                height: size.height * 0.3,
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    childCount: controller.heroList.length,
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
