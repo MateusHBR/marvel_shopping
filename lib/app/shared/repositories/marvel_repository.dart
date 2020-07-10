fad2bc33248bea1c8d8395a4eff95c46400e75c4
MateusHBR <mateusmorais.r@gmail.com>
2020-07-10 19:00:54 -0300

    acessando  a api da marvel e adicionando uma variavel offset

--- lib/app/shared/repositories/marvel_repository.dart
+++ lib/app/shared/repositories/marvel_repository.dart
@@ -6,13 +6,12 @@ class MarvelRepository {
 
   MarvelRepository(this._dio);
 
-  Future<List<HeroModel>> getHeroes() async {
-    var response = await _dio.get("");
+  Future<List<HeroModel>> getHeroes({offset = 0}) async {
+    var response = await _dio.get("&offset=$offset");
     List<HeroModel> heroes = [];
 
     for (var json in (response.data["data"]["results"] as List)) {
       var hero = HeroModel.fromJson(json);
-      print(hero.url);
       heroes.add(hero);
     }
 
