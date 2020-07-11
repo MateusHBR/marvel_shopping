db68ef6301274ecb776be60b64eb00505370edd0
MateusHBR <mateusmorais.r@gmail.com>
2020-07-11 16:52:11 -0300

    injetando dependências

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,19 +1,31 @@
-import 'package:marvel_store/app/shared/repositories/marvel_repository.dart';
-
-import 'home_controller.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:dio/dio.dart';
-import 'home_page.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'package:marvel_store/app/shared/repositories/marvel_repository.dart';
+import 'package:marvel_store/app/shared/stores/carrinho_store.dart';
+
+import './home_controller.dart';
+import './pages/item_page/item_controller.dart';
+import './pages/item_page/item_page.dart';
+import './home_page.dart';
 
 class HomeModule extends ChildModule {
   @override
   List<Bind> get binds => [
+        Bind((i) => CarrinhoStore()),
+        Bind((i) => ItemController(i.get<CarrinhoStore>())),
         Bind((i) => HomeController(i.get<MarvelRepository>())),
       ];
 
   @override
   List<Router> get routers => [
         Router(Modular.initialRoute, child: (_, args) => HomePage()),
+        Router(
+          '/item',
+          child: (_, args) => ItemPage(
+            hero: args.data,
+          ),
+        ),
       ];
 
   static Inject get to => Inject<HomeModule>.of();
