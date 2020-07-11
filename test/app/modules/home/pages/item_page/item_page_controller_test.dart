626130d996af46a68dd825026c59fbaa103e7536
MateusHBR <mateusmorais.r@gmail.com>
2020-07-11 16:50:41 -0300

    libs e arquivos de test

---
+++ test/app/modules/home/pages/item_page/item_page_controller_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_modular/flutter_modular_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:marvel_store/app/modules/home/pages/item_page/item_page_controller.dart';
+import 'package:marvel_store/app/modules/home/home_module.dart';
+
+void main() {
+  initModule(HomeModule());
+  // ItemPageController itempage;
+  //
+  setUp(() {
+    //     itempage = HomeModule.to.get<ItemPageController>();
+  });
+
+  group('ItemPageController Test', () {
+    //   test("First Test", () {
+    //     expect(itempage, isInstanceOf<ItemPageController>());
+    //   });
+
+    //   test("Set Value", () {
+    //     expect(itempage.value, equals(0));
+    //     itempage.increment();
+    //     expect(itempage.value, equals(1));
+    //   });
+  });
+}
---
+++ test/app/modules/home/pages/item_page/item_page_page_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_modular/flutter_modular_test.dart';
+
+import 'package:marvel_store/app/modules/home/pages/item_page/item_page_page.dart';
+
+void main() {
+  testWidgets('ItemPagePage has title', (tester) async {
+    //  await tester.pumpWidget(buildTestableWidget(ItemPagePage(title: 'ItemPage')));
+    //  final titleFinder = find.text('ItemPage');
+    //  expect(titleFinder, findsOneWidget);
+  });
+}
