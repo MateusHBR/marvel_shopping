c8066fc340933f6c1943b347ad32be075c9d32a4
MateusHBR <mateusmorais.r@gmail.com>
2020-07-13 14:42:03 -0300

    adicionando snackbar global

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import './shared/utils/global_scaffold.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) {
+        return Scaffold(
+          key: GlobalScaffold.instance.scaffKey,
+          body: child,
+        );
+      },
       navigatorKey: Modular.navigatorKey,
       title: 'Flutter Slidy',
       debugShowCheckedModeBanner: false,
