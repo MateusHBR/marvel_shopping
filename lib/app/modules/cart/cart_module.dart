8f6efe68767842c6594620025dfd2d78c524a4ff
MateusHBR <mateusmorais.r@gmail.com>
2020-07-13 17:24:29 -0300

    modulo de carrinho com injeção de dependências

---
+++ lib/app/modules/cart/cart_module.dart
@@ -0,0 +1,20 @@
+import 'package:marvel_store/app/shared/stores/carrinho_store.dart';
+
+import 'cart_controller.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:dio/dio.dart';
+import 'cart_page.dart';
+
+class CartModule extends ChildModule {
+  @override
+  List<Bind> get binds => [
+        Bind((i) => CartController(i.get<CarrinhoStore>())),
+      ];
+
+  @override
+  List<Router> get routers => [
+        Router(Modular.initialRoute, child: (_, args) => CartPage()),
+      ];
+
+  static Inject get to => Inject<CartModule>.of();
+}
