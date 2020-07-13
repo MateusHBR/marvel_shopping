13a75e4be4516d6441cc2563bbb9225493c2eb4d
MateusHBR <mateusmorais.r@gmail.com>
2020-07-13 14:53:43 -0300

    criando drawer que pode ser reutilizado

--- lib/app/shared/components/drawer_component.dart
+++ lib/app/shared/components/drawer_component.dart
@@ -11,81 +11,75 @@ class DrawerComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
 
-    return Drawer(child: Observer(
-      builder: (_) {
-        if (!carrinhoStore.loggedIn) {
-          return Column(
-            children: <Widget>[
-              UserLoggedOffComponent(
-                function: carrinhoStore.logIn,
-              ),
-              Container(),
-            ],
-          );
-        } else {
-          return Column(
-            children: <Widget>[
-              DrawerHeader(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Container(
-                      height: 70,
-                      width: 70,
-                      child: CircleAvatar(
-                        backgroundColor: Colors.black,
-                        foregroundColor: Colors.white,
-                        child: Icon(
-                          Icons.person_outline,
-                          size: 40,
+    return Drawer(
+      child: Observer(
+        builder: (_) {
+          if (!carrinhoStore.loggedIn) {
+            return Column(
+              children: <Widget>[
+                UserLoggedOffComponent(
+                  function: () {
+                    Modular.to.pushNamed('/login');
+                  },
+                  // function: carrinhoStore.logIn,
+                ),
+                Container(),
+              ],
+            );
+          } else {
+            return Column(
+              children: <Widget>[
+                DrawerHeader(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                        height: 70,
+                        width: 70,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.black,
+                          foregroundColor: Colors.white,
+                          child: Icon(
+                            Icons.person_outline,
+                            size: 40,
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text('Bem-Vindo ${carrinhoStore.user.name}!'),
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Text('Bem-Vindo ${carrinhoStore.user.name}!'),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              ListTile(
-                onTap: () {},
-                leading: Icon(Icons.shopping_cart),
-                title: Text('Carrinho'),
-                trailing: Icon(Icons.arrow_forward_ios),
-              ),
-              Divider(),
-              ListTile(
-                onTap: carrinhoStore.logOut,
-                leading: Icon(
-                  Icons.close,
-                  color: Colors.red,
+                ListTile(
+                  onTap: () {},
+                  leading: Icon(Icons.shopping_cart),
+                  title: Text('Carrinho'),
+                  trailing: Icon(Icons.arrow_forward_ios),
                 ),
-                title: Text(
-                  'Sair',
-                  style: TextStyle(color: Colors.red),
+                Divider(),
+                ListTile(
+                  onTap: carrinhoStore.logOut,
+                  leading: Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    'Sair',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  trailing: Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.red,
+                  ),
                 ),
-                trailing: Icon(
-                  Icons.arrow_forward_ios,
-                  color: Colors.red,
-                ),
-              ),
-              Divider(),
-            ],
-          );
-        }
-      },
-    )
-        // ListView(
-        //   children: <Widget>[
-        //     Observer(
-        //       builder: (_) {
-
-        //       },
-        //     ),
-
-        //   ],
-        // ),
-        );
+                Divider(),
+              ],
+            );
+          }
+        },
+      ),
+    );
   }
 }
