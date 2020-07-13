d017236ca200479d13f043d1a49bcc8a1e3150d3
MateusHBR <mateusmorais.r@gmail.com>
2020-07-13 17:28:16 -0300

    adicionando item ao carrinho

--- lib/app/modules/home/pages/item_page/item_controller.dart
+++ lib/app/modules/home/pages/item_page/item_controller.dart
@@ -1,3 +1,5 @@
+import 'package:marvel_store/app/shared/models/carrinho_hero_model.dart';
+import 'package:marvel_store/app/shared/models/hero_model.dart';
 import 'package:mobx/mobx.dart';
 import 'package:marvel_store/app/shared/stores/carrinho_store.dart';
 part 'item_controller.g.dart';
@@ -8,4 +10,17 @@ abstract class _ItemControllerBase with Store {
   final CarrinhoStore _carrinhoStore;
 
   _ItemControllerBase(this._carrinhoStore);
+
+  @computed
+  bool get userLogged => _carrinhoStore.user.isLoggedIn;
+
+  @action
+  addInCart(HeroModel hero) {
+    _carrinhoStore.addInCart(
+      CarrinhoHeroModel(
+        hero: hero,
+        quantity: 1,
+      ),
+    );
+  }
 }
--- lib/app/modules/home/pages/item_page/item_controller.g.dart
+++ lib/app/modules/home/pages/item_page/item_controller.g.dart
@@ -9,10 +9,32 @@ part of 'item_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$ItemController on _ItemControllerBase, Store {
+  Computed<bool> _$userLoggedComputed;
+
+  @override
+  bool get userLogged =>
+      (_$userLoggedComputed ??= Computed<bool>(() => super.userLogged,
+              name: '_ItemControllerBase.userLogged'))
+          .value;
+
+  final _$_ItemControllerBaseActionController =
+      ActionController(name: '_ItemControllerBase');
+
+  @override
+  dynamic addInCart(HeroModel hero) {
+    final _$actionInfo = _$_ItemControllerBaseActionController.startAction(
+        name: '_ItemControllerBase.addInCart');
+    try {
+      return super.addInCart(hero);
+    } finally {
+      _$_ItemControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+userLogged: ${userLogged}
     ''';
   }
 }
